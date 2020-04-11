0ba27582fff6581781cc8aa96e6366187ff7f6b7
alexdeisem <semionchik.a.g@gmail.com>
2020-04-11 23:33:57 +0300

    Setup Light Bootstrap Dashboard Laravel unnecessary migrations removed

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Frontend Presets Providers...
+         */
+        LaravelFrontendPresets\LightBootstrapPreset\LightBootstrapPresetServiceProvider::class
+
     ],
 
     /*
